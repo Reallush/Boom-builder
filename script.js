1e1699618fce569972684b5ecf19f3d5387f5081
Reallush <reallush@gmail.com>
2025-02-25 20:28:00 +0700

    first update

---
+++ script.js
@@ -0,0 +1,120 @@
+// this area is for general translation of the page. 
+const translations = {
+    en: {
+        //navbar
+        hello: "Welcome to Boom builders! ",
+        content: " The Knockdown House Experts! Are you looking for a stylish, durable, and affordable home that’s easy to set up? At, we specialize in high-quality knockdown houses that combine modern design with convenience. Whether you need a compact home, a resort-style villa, or an office space, we have the perfect solution for you.",
+        ordr: '<button type="button" class="btn dbtn btn-outline-light btn-sm"><i class="fa-regular fa-clipboard"></i> Order form</button>',
+        loc: '<button type="button" class="btn dbtn btn-outline-light btn-sm"><i class="fa-solid fa-fish"></i> Locations</button>',
+        bout: '<button type="button" class="btn dbtn btn-outline-light btn-sm"><i class="fa-regular fa-clipboard"></i> About us</button>',
+
+        index: {
+            heading: "Welcome to our website!",
+            ordr2: '<button type="button" class="btn dbtn btn-outline-light btn-lg"><i class="fa-regular fa-clipboard"></i> Order Page</button>',
+            entr: '<button type="button" class="btn introbtn dbtn btn-secondary btn-lg"><i class="fa-solid fa-arrow-right-to-bracket"></i> Enter page</button>',
+        },
+        aboutus: {
+            welcom: "Welcome to all sushi lovers.",
+            introtext: "For over 10 years, <br> our family has proudly served handcrafted sushi at the local market. <br><br> Each piece is made with the freshest ingredients <br>and a passion for authentic flavors. <br><br> From classic favorites to innovative creations,<br> our menu offers something for everyone. <br><br> As a family-run business, we pour our heart and soul into every roll,<br> ensuring you get the best sushi experience. <br><br> Visit us at the market and taste the difference that family dedication makes. <br> We look forward to sharing our love for sushi with you!",
+            getsu: "Getting some sushi.",
+            getsushi: "At our stall you can pick up one of the plates to select your sushi <br>once done, we will box it for you to take it home<br><br>Alternatively, you can ask us for a paper plate should you wish to eat it at the market.",
+        },
+        locations: {
+            cardbigc: 'Big C Ban Tai',
+            cardbigc2: 'Monday, Wednesday and Friday from 15:00-19:00.',
+            cardts: 'Walking street Thong Sala',
+            cardts2: 'Every Saturday, across the Zen. 16:00-21:00.',
+            cardck: 'Chaloklum walking street',
+            cardck2: 'Every Sunday on Chaloklum pier. 16:00-21:00',
+            dir:'Directions',
+            dir1:'Directions',
+            dir2:'Directions',
+        },
+        catalog: {
+            totop:'<button type="button" class="btn dbtn btn-outline-light btn-md"><i class="fa"></i> To top of page</button>',
+        },
+        //           `${getIngredients ('en','r',)}`,
+
+    },
+    th: {
+        //navbar
+        hello: "สวัสดีครับ",
+        content: "**ผู้เชี่ยวชาญด้านบ้านน็อคดาวน์!**    คุณกำลังมองหาบ้านที่มี **ดีไซน์สวย ทนทาน และราคาประหยัด** ที่สามารถติดตั้งได้ง่ายใช่หรือไม่?          ที่ **[ชื่อบริษัทของคุณ]** เราเชี่ยวชาญในการสร้าง **บ้านน็อคดาวน์คุณภาพสูง** ที่ผสมผสาน **การออกแบบที่ทันสมัยและความสะดวกสบาย**          ไม่ว่าคุณจะต้องการ **บ้านขนาดกะทัดรัด, วิลล่าสไตล์รีสอร์ท หรือ พื้นที่สำนักงาน** เรามีโซลูชันที่สมบูรณ์แบบสำหรับคุณ!",
+        ordr: '<button type="button" class="btn dbtn btn-outline-light btn-sm"><i class="fa-regular fa-clipboard"></i> แบบฟอร์มสั่งซื้อ</button>',
+        loc: '<button type="button" class="btn dbtn btn-outline-light btn-sm"><i class="fa-solid fa-fish"></i> สถานที่</button>',
+        bout: '<button type="button" class="btn dbtn btn-outline-light btn-sm"><i class="fa-regular fa-clipboard"></i> เกี่ยวกับเรา</button>',
+        index: {
+            ordr2: '<button type="button" class="btn dbtn btn-outline-light "><i class="fa-regular fa-clipboard"></i> แบบฟอร์มสั่งซื้อ</button>',
+            entr: '<button type="button" class="btn introbtn dbtn btn-secondary "><i class="fa-solid fa-arrow-right-to-bracket"></i> เข้าสู่หน้าเว็บ</button>',
+            heading: "ยินดีตอนรับ",
+        },
+        aboutus: {
+            welcom: "ยินดีต้อนรับคนรักซูชิทุกคน",
+            introtext: "กว่า 10 ปีที่ครอบครัวของเราได้ภูมิใจนำเสนอซูชิที่ทำด้วยมือที่ตลาดท้องถิ่น<br>ทุกชิ้นทำจากวัตถุดิบที่สดใหม่ที่สุดและด้วยความหลงใหลในรสชาติที่แท้จริง<br>จากเมนูคลาสสิกยอดนิยมถึงการสร้างสรรค์ใหม่ ๆ <br>เมนูของเรามีบางสิ่งสำหรับทุกคนในฐานะธุรกิจที่ดำเนินงานโดยครอบครัว <br>เราใส่ใจและตั้งใจทำทุกม้วน <br>เพื่อให้คุณได้รับประสบการณ์ซูชิที่ดีที่สุดมาเยี่ยมชมเราที่ตลาดและสัมผัสถึง<br>ความแตกต่างที่มาจากการใส่ใจของครอบครัวเรา<br>หวังว่าจะได้แบ่งปันความรักที่เรามีต่อซูชิกับคุณ!",
+            getsu: "การซื้อซูชิ",
+            getsushi: "ที่ร้านของเรา คุณสามารถหยิบจานเพื่อเลือกซูชิของคุณ <br>เมื่อเสร็จแล้ว เราจะบรรจุใส่กล่องให้คุณนำกลับบ้าน<br><br>นอกจากนี้ คุณยังสามารถขอจานกระดาษจากเราได้หากต้องการทานที่ตลาด",
+        },
+        locations: {
+            cardbigc: 'ตลาดบิ๊กซี',
+            cardbigc2: 'วันจันทร์, วันพุธ และวันศุกร์ ตั้งแต่เวลา 15:00-19:00.',
+            cardts: 'ถนนคนเดินทองศาลา',
+            cardts2: 'ทุกวันเสาร์, ตรงข้าม Zen. 16:00-21:00.',
+            cardck: 'ถนนคนเดินโฉลกหลำ',
+            cardck2: 'ทุกวันอาทิตย์ที่ท่าเรือโฉลกหลำ. 16:00-21:00',
+            dir:'นำทาง',
+            dir1:'นำทาง',
+            dir2:'นำทาง',
+        },
+        catalog: {
+            totop:'<button type="button" class="btn dbtn btn-outline-light btn-md"><i class="fa"></i>ไปที่ด้านบนของเพจ</button>',
+        },
+
+    },
+};
+
+// Function to change the language
+function changeLanguage(language, page) {
+    const pageTranslations = translations[language][page];
+
+
+
+
+    // Rest of your JavaScript code...
+
+    //navbar language
+    document.getElementById('hello').textContent = translations[language].hello;
+    document.getElementById('content').innerHTML = translations[language].content;
+    document.getElementById('ordr').innerHTML = translations[language].ordr;
+    document.getElementById('loc').innerHTML = translations[language].loc;
+    document.getElementById('bout').innerHTML = translations[language].bout;
+
+
+    //the rest of the page
+    for (const key in pageTranslations) {
+        if (pageTranslations.hasOwnProperty(key)) {
+            document.getElementById(key).innerHTML = pageTranslations[key];
+        }
+
+    }
+
+    localStorage.setItem('preferredLanguage', language);
+    localStorage.setItem('preferredPage', page);
+}
+
+
+
+// Event listener for the language select dropdown
+document.getElementById('languageSelect').addEventListener('change', function () {
+    const page = document.body.dataset.page;
+    changeLanguage(this.value, page);
+});
+
+
+
+// Retrieve preferred language from local storage and set it
+document.addEventListener('DOMContentLoaded', (event) => {
+    const savedLanguage = localStorage.getItem('preferredLanguage') || 'en';
+    const page = document.body.dataset.page;
+    document.getElementById('languageSelect').value = savedLanguage;
+    changeLanguage(savedLanguage, page);
+});
